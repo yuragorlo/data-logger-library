074fd0a82c1249f8177b05498298db5fc32e6a37
Yura Gorlo <ygorlo@newagesol.co>
2022-04-13 16:21:44 +0300

    test6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     author='New Age Sol.',
     author_email='dev@newagesol.com',
-    name='logger',
+    name='logger_library',
     version='1.0',
     description='HRZN iGaming platform data team logger.',
     url='https://github.com/yuragorlo/data-logger-library.git',
